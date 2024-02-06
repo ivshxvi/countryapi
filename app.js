3d15e1241cec239e042c9a4c8f54146695d90640
ivshxvi <ivhsxvi@gmail.com>
2024-02-06 10:39:54 +0000

    Add getting country by name, creating and deleting.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cr = require("./routers/country-router")
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 app.use(logger)
 
 app.use("/countries", cr)
--- models/Country.js
+++ models/Country.js
@@ -17,7 +17,32 @@ class Country {
             throw new Error("No countries the database")
         }
 
-        return response.rows
+        return response.rows.map(c => new Country(c))
+    }
+
+    static async getOneByCountryName(cn) {
+        const response = await db.query("SELECT * FROM country WHERE LOWER(name) = LOWER($1);", [cn])
+        if (response.rows.length === 0){
+            throw new Error("Can not get country")
+        }
+
+        return new Country(response.rows[0])
+    }
+
+    static async create(data) {
+        const {name, capital, population, languages} = data
+        const ec = await db.query("SELECT name FROM country WHERE LOWER(name) = LOWER($1);",[name])
+        if(ec.rows.length > 0){
+            throw new Error("Country already is in the DB")
+        }
+
+        let response = await db.query("INSERT INTO country (name, capital, population, languages) VALUES ($1, $2, $3, $4) RETURNING *;", [name, capital, population, languages])
+        return new Country(response.rows[0])
+    }
+
+    async destroy() {
+        let response = await db.query("DELETE FROM country WHERE name = $1 RETURNING *", [this.name])
+        return new Country(response.rows[0])
     }
   }
   
